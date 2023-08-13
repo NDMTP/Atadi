ca6812079e710f1e4264bef8d3acf61e1688896d
Trần Ngọc Duy <duybii922002@gmail.com>
2023-08-13 18:28:48 +0700

    dang chinh code dangnhap

---
+++ dangnhap.php
@@ -0,0 +1,40 @@
+<?php
+
+include("connect.php");
+                if(isset($_POST["sb1"])){
+                    
+                    $sql="SELECT * FROM nguoidung where email='".$_POST["email"]."' AND matkhau='".md5($_POST["psw1"])."'";
+                    $result1 = $conn->query($sql);
+                  // print_r($result1);
+                    if($result1->num_rows>0){
+                        
+                        $row = $result1->fetch_assoc();
+                        
+                        session_start();
+                        $_SESSION["email"] = $row["EMAIL"];
+                        $_SESSION["matkhau"]=$row["MATKHAU"];
+                        $_SESSION["diachi"]=$row["DIACHI"];
+                        $_SESSION["hoten"]=$row["TEN"];
+                        $_SESSION["sdt"]=$row["SDT"];
+                   
+                        header('Location: index.php');
+                     
+/*
+                        echo $_SESSION["name"]."<br>==";
+                        echo $_SESSION["ngaysinh"]."<br>==";
+                        echo $_SESSION["sdt"];
+                        
+    */ 
+                    }
+                    else{
+                 
+                        echo '<script language="javascript">
+                    alert("Nhập sai email hoặc mật khẩu.");
+                    history.back();
+                     </script>';
+                      
+                    }
+                   
+                    }
+                
+                ?>
