d7e81300665d6fabfcb8a9359070e946caaf2e63
Trần Ngọc Duy <duybii922002@gmail.com>
2023-08-13 19:04:49 +0700

    xong đăng nhập đăng kí

--- connect.php
+++ connect.php
@@ -8,6 +8,7 @@ $dbname = "qlbanmicay";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-}
+  
+} 
 
 ?>
